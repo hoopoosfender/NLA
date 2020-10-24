be0f6692447b3b67d0918210fc94ba241989cb19
Ben Tu <hoopoos@gmail.com>
2020-10-25 02:01:50 +1100

    add foreign key

--- src/main/java/com/nla/controller/BookController.java
+++ src/main/java/com/nla/controller/BookController.java
@@ -1,22 +1,30 @@
 package com.nla.controller;
 
+import com.nla.model.People;
 import com.nla.model.Book;
-import com.nla.repository.BookRepository;
+import com.nla.repository.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Optional;
 import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 public class BookController {
 
     @Autowired
-    private BookRepository bookRepository;
+    private PeopleRepository peopleRepository;
 
-    @GetMapping(path = "/books", produces = "application/json; charset=UTF-8")
-    public List<Book> feed() {
-        return this.bookRepository.findAll();
+    @GetMapping(value = "/peoplebooks/{personId}",  produces = "application/json; charset=UTF-8")
+    public List<Book> getBooks(@PathVariable long personId) {
+        Optional<People> people =  this.peopleRepository.findById(personId);
+        if (people.isPresent()) {
+            return people.get().getBooks();
+        } else {
+            return new ArrayList<Book>();
+        }
     }
 
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,35 +1,39 @@
-/*
-
-$( document ).ready(function() {
-  let endpoint = 'books'
-
-var commentTable = document.getElementById("comments");
-
+$( "#showbooks" ).click(function() {
+    var id = $(this).attr('name');
+    var booksTable = document.getElementById("books");
+    var delay = 2000;
     $.ajax({
-        url: endpoint,
+        url: "/peoplebooks/"+ id,
         contentType: "application/json",
         dataType: 'json',
         success: function(result){
-            console.log(result);
+        console.log(result);
+        setTimeout(function() {
+          $(".loader").removeClass('is-active');
+        }, delay);
 
                        var book = JSON.parse(JSON.stringify(result));
 
                        $.each( book, function( index, value ){
-                         var row = commentTable.getElementsByTagName("tbody")[0].insertRow(index);
+/*                         var row = booksTable.getElementsByTagName("tbody")[0].insertRow(index);
                          var cell0 = row.insertCell(0);
                          var cell1 = row.insertCell(1);
-
+                         var cell2 = row.insertCell(2);
                          cell0.className = "author-style";
-                         cell0.innerHTML = value.id;
-
+                         cell0.innerHTML = value.isbn;
                          cell1.className = "text";
-                         cell1.innerHTML = value.name;
-
+                         cell1.innerHTML = value.title;
+                         cell2.className = "text";
+                         cell2.innerHTML = value.author;*/
                        });
 
         }
     })
 
+
 });
-*/
+
+$(document).ajaxStart(function() {
+    $(".loader").addClass('is-active');
+    });
 
