a5d7163f4782374922bb662b0374f696bdcf8e39
Ben Tu <hoopoos@gmail.com>
2020-10-24 22:25:45 +1100

    add data access

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,6 +16,15 @@ CREATE TABLE PEOPLE (
   phone VARCHAR(250) DEFAULT NULL
 );
 
+
+DROP TABLE IF EXISTS PEOPLE_BOOK;
+
+CREATE TABLE PEOPLE_BOOK (
+  people_id int NOT NULL,
+  books_id int NOT NULL
+);
+
+
 INSERT INTO BOOK (author, title, ISBN) VALUES
 ('Aliko Dangote', 'Billionaire Industrialist', 'ISBN0001'),
 ('Bill Gates', 'Billionaire Tech Entrepreneur','ISBN1001'),
@@ -25,3 +34,10 @@ INSERT INTO PEOPLE (name, email, phone) VALUES
 ('Aliko Dangote', 'a@gmail.com', '0423666'),
 ('Bill Gates', 'b@gmail.com','0422233'),
 ('Folrunsho Alakija', 'c@gmail.com','02444232');
+
+INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
+(1,1);
+INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
+(1,2);
+INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
+(1,3);
