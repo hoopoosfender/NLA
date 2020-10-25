907da70651f4d543c3a101320e57a897546dff02
Ben Tu <hoopoos@gmail.com>
2020-10-25 16:28:18 +1100

    add paging

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,24 +3,27 @@ INSERT INTO BOOK (author, title, ISBN) VALUES
 ('Bill Gates', 'Billionaire Tech Entrepreneur','ISBN1001'),
 ('Tom Hanks', 'Starcraft','ISBN2001'),
 ('James Brown', 'Funky!','ISBN3001'),
-('Alex Peter', 'Summer Time','ISBN4001');
+('Alex Peter', 'Summer Time','ISBN5001'),
+('Tom Hanks', 'Starcraft 2','ISBN6001'),
+('James Brown', 'Funky Again','ISBN7001'),
+('Alex Peter', 'Spring Time','ISBN8001');
 
 INSERT INTO PEOPLE (name, email, phone) VALUES
 ('Aliko Dangote', 'a@gmail.com', '0423666'),
 ('Bill Gates', 'b@gmail.com','0422233'),
 ('Folrunsho Alakija', 'c@gmail.com','02444232'),
-('Aliko Dangote', 'a@gmail.com', '0423666'),
-('Bill Gates', 'b@gmail.com','0422233'),
-('Folrunsho Alakija', 'c@gmail.com','02444232'),
-('Aliko Dangote', 'a@gmail.com', '0423666'),
-('Bill Gates', 'b@gmail.com','0422233'),
-('Folrunsho Alakija', 'c@gmail.com','02444232'),
-('Aliko Dangote', 'a@gmail.com', '0423666'),
-('Bill Gates', 'b@gmail.com','0422233'),
-('Folrunsho Alakija', 'c@gmail.com','02444232'),
-('Aliko Dangote', 'a@gmail.com', '0423666'),
-('Bill Gates', 'b@gmail.com','0422233'),
-('Folrunsho Alakija', 'c@gmail.com','02444232');
+('Jack Dangote', 'a@gmail.com', '0423666'),
+('Peter Gates', 'b@gmail.com','0422233'),
+('Alex Alakija', 'c@gmail.com','02444232'),
+('Lee Dangote', 'a@gmail.com', '0423666'),
+('William Gates', 'b@gmail.com','0422233'),
+('Mat Alakija', 'c@gmail.com','02444232'),
+('David Dangote', 'a@gmail.com', '0423666'),
+('Lily Gates', 'b@gmail.com','0422233'),
+('Julie Alakija', 'c@gmail.com','02444232'),
+('Sam Dangote', 'a@gmail.com', '0423666'),
+('Louis Gates', 'b@gmail.com','0422233'),
+('Smith Alakija', 'c@gmail.com','02444232');
 
 INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
 (1,1);
@@ -33,3 +36,10 @@ INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
 (2,2);
 INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
 (2,3);
+
+INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
+(4,1);
+INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
+(5,2);
+INSERT INTO PEOPLE_BOOK (people_id, books_id) VALUES
+(7,3);
